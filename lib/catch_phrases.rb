018ba8156fc6d5c2df9299cf26bbe083db19dc12
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-08 18:31:48 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,6 +13,10 @@ def link
   puts phrase1
 end
 
+phrase = 'It\'s-a me, Mario!'
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+phrase1 = "It's Dangerous To Go Alone! Take This."
+
 def all_phrases mario, toadstool, link
   puts mario 
   puts toadstool 
